c463d92b96f0131717745f78f98eb277298fa483
hite <iamhite@gmail.com>
2018-12-27 17:12:43 +0800

    Initial Commit

---
+++ AppHost/AppHost.h
@@ -0,0 +1,19 @@
+//
+//  AppHost.h
+//  AppHost
+//
+//  Created by liang on 2018/12/27.
+//  Copyright © 2018 liang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AppHost.
+FOUNDATION_EXPORT double AppHostVersionNumber;
+
+//! Project version string for AppHost.
+FOUNDATION_EXPORT const unsigned char AppHostVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AppHost/PublicHeader.h>
+
+
