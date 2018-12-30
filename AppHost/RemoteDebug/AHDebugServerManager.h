a3ea72bbf269dcdccf0e685e359fc50007c0262f
hite <iamhite@gmail.com>
2018-12-30 20:11:50 +0800

    add: 实现如何远程调试 appHost 接口

---
+++ AppHost/RemoteDebug/AHDebugServerManager.h
@@ -0,0 +1,17 @@
+//
+//  AHDebugServerManager.h
+//  AppHost
+//
+//  Created by liang on 2018/12/29.
+//  Copyright © 2018 liang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AHDebugServerManager : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ AppHost/RemoteDebug/AHDebugServerManager.m
@@ -0,0 +1,13 @@
+//
+//  AHDebugServerManager.m
+//  AppHost
+//
+//  Created by liang on 2018/12/29.
+//  Copyright © 2018 liang. All rights reserved.
+//
+
+#import "AHDebugServerManager.h"
+
+@implementation AHDebugServerManager
+
+@end
